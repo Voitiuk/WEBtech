3ee53dd959c7a6ef98a5bfc8d2677f10e2848639
Voitiuk <st6879001@stud.duikt.edu.ua>
2024-12-11 00:06:47 +0200

    1

---
+++ prac6/script.js
@@ -0,0 +1,6 @@
+const pokemon = document.getElementById('pokemon');
+
+// Додаткові анімації або інтерактивність можна додати тут
+pokemon.addEventListener('click', () => {
+    alert('You clicked on Pikachu!');
+});
